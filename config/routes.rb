b61970886579786b5c8fa046aaa358bd69bd2818
utdmn2 <utdmn2@gmail.com>
2021-09-09 08:02:53 +0000

    [Add]編集画面

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ root to: 'homes#top'
 post 'books' => 'books#create'
 get 'books' => 'books#index'
 get 'books/:id' => 'books#show', as: 'book'
+get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+patch 'books/:id' => 'books#update', as: 'update_book'
 
 end
