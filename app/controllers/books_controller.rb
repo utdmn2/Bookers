2144d3c7d3baaccd18f8a31e3f5d8ee3bf38c972
utdmn2 <utdmn2@gmail.com>
2021-09-09 02:06:02 +0000

    [Add]投稿フォーム

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,16 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @book = Book.new(book_params)
+  end
+
+
+  def create
+  #データを新規登録するためのインスタンス作成
+   book = Book.new(book_params)
+  #データをデータベースに保存するためのsaveメソッド実行
+   book.save
+   #redirect先はどこにするか
   end
 
   def show
@@ -7,4 +18,11 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html>
 root to: 'homes#top'
 resources :books
+post 'books' => 'books#create'
+get 'books' => 'books#index'
+
 
 end
