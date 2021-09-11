3445380f21523a888452ae0da2f677fa3c1d9fba
utdmn2 <utdmn2@gmail.com>
2021-09-11 06:37:42 +0000

    [Add]新規投稿バリテーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,9 @@ class BooksController < ApplicationController
 
   def create
    @book = Book.new(book_params)
+   @books = Book.all
    if @book.save
-     redirect_to book_path(@book.id)
+     redirect_to book_path(@book.id),notice: 'Book was successfully created.'
    else
      render :index
    end
